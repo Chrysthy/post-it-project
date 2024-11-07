b8f636a543ebf171f62007887947c6b693a5780b
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-07 20:28:56 -0300

    add const id crypto

--- index.js
+++ index.js
@@ -11,10 +11,29 @@ app.get('/note/:id', (req, res) => {
     res.sendFile(__dirname + '/public/note.html');
 })
 
-app.post('/note', (req, res) => {
+app.post('/note', async (req, res) => {
     const { content } = req.body;
+
+    if (!content) {
+        return res.send('<span> Erro inesperado</span>')
+    }
+
+    const id = crypto.randomUUID();
+    await saveNotes(id, content);
+
+    res.send(`
+        
+        <p>Share your note through the link
+
+            <br>
+
+            <span>${req.headers.origin}/note/${id}}</span>
+
+        </p>`);
+
+
 });
 
-const PORT = 3000;app.listen(PORT, () => {
+const PORT = 3000; app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
