fb5d42fd4e75a0d92b808b4d83801f140c5cfb6b
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-06 21:31:47 -0300

    create app.use

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.sendFile(__dirname + 'index.html');
 });
 
 const PORT = 3000;app.listen(PORT, () => {
